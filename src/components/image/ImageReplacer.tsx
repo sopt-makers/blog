6c7d0bd42b9032d332cf9c9b0fae127776425621
Tekiter <tekiter8@gmail.com>
2023-06-10 03:44:26 +0900

    feat: 이미지 블러 수정

--- src/components/image/ImageReplacer.tsx
+++ src/components/image/ImageReplacer.tsx
@@ -6,10 +6,13 @@ import { FC, ImgHTMLAttributes, useEffect, useState } from 'react';
 interface ImageReplacerProps extends ImgHTMLAttributes<HTMLImageElement> {
   src: string;
   preview: string;
+  width: number;
+  height: number;
 }
 
-const ImageReplacer: FC<ImageReplacerProps> = ({ src, preview, alt, className, ...props }) => {
+const ImageReplacer: FC<ImageReplacerProps> = ({ src, preview, alt, className, width, height, ...props }) => {
   const [loaded, setLoaded] = useState(false);
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
     const img = new Image();
@@ -17,6 +20,7 @@ const ImageReplacer: FC<ImageReplacerProps> = ({ src, preview, alt, className, .
 
     const loaded = () => {
       setLoaded(true);
+      console.log('Loaded!');
     };
     img.addEventListener('load', loaded);
 
@@ -25,11 +29,25 @@ const ImageReplacer: FC<ImageReplacerProps> = ({ src, preview, alt, className, .
     };
   }, [src]);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAnimate(true);
+      console.log('ANIMATE!');
+    }, 500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <img
       src={loaded ? src : preview}
       alt={alt}
-      className={clsx(!loaded && 'blur-md', 'transition-[filter] duration-100', className)}
+      className={clsx(!loaded && 'blur', animate && 'transition-[filter] duration-150')}
+      style={{ aspectRatio: `${width} / ${height}` }}
+      width={width}
+      height={height}
       {...props}
     />
   );
--- src/components/image/sizing.ts
+++ src/components/image/sizing.ts
@@ -6,11 +6,15 @@ export async function getImageSize(filePath: string) {
   const image = sharp(filePath);
   const { width, height } = await image.metadata();
 
+  if (!width || !height) {
+    throw new Error(`Failed to get image size: ${filePath}`);
+  }
+
   return { width, height };
 }
 
 export async function getPreviewImage(filePath: string): Promise<string> {
-  const outputBuffer = await sharp(filePath).resize(50).blur().png({ quality: 10 }).toBuffer();
+  const outputBuffer = await sharp(filePath).resize(50).png({ quality: 10 }).toBuffer();
   const base64 = outputBuffer.toString('base64');
   return `data:image/png;base64,${base64}`;
 }
