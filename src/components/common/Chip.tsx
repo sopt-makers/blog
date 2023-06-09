c6cfb02ad668aa4a23e561c827c6a25a24b4bf89
Tekiter <tekiter8@gmail.com>
2023-06-10 04:16:07 +0900

    feat: 카테고리 구현

--- src/components/common/Chip.tsx
+++ src/components/common/Chip.tsx
@@ -10,9 +10,8 @@ const Chip: FC<ChipProps> = ({ children, active = false }) => {
   return (
     <div
       className={clsx(
-        'cursor-pointer rounded-[12px] border border-black40 bg-black80 px-[12px] py-[8px] text-[16px] text-gray10 transition',
-        'hover:bg-black60',
-        active && 'border-gray-10 bg-gray10 text-black100',
+        'cursor-pointer rounded-[12px] border px-[12px] py-[8px] text-[16px] transition',
+        active ? 'border-gray10 bg-gray10 text-black100' : 'border-black40 bg-black80 text-gray10 hover:bg-black60',
       )}
     >
       {children}
