cac6d064c562177eb9d35a581e3bd7258110b0cd
Tekiter <tekiter8@gmail.com>
2023-06-14 03:12:11 +0900

    feat: 폰트 버그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,28 @@ import { Roboto_Mono } from 'next/font/google';
 import localFont from 'next/font/local';
 
 const suitFont = localFont({
-  src: './SUIT-Variable.woff2',
+  src: [
+    {
+      path: './SUIT-Light.woff2',
+      weight: '300',
+    },
+    {
+      path: './SUIT-Regular.woff2',
+      weight: '400',
+    },
+    {
+      path: './SUIT-Medium.woff2',
+      weight: '500',
+    },
+    {
+      path: './SUIT-SemiBold.woff2',
+      weight: '600',
+    },
+    {
+      path: './SUIT-Bold.woff2',
+      weight: '700',
+    },
+  ],
   display: 'swap',
   variable: '--font-suit',
 });
