96cb5f51a7ee95733f9da091b20ea0d1e2506c16
Tekiter <tekiter8@gmail.com>
2023-06-21 18:18:30 +0900

    feat: 메타데이터 위치 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,33 @@ import './globals.css';
 import { Roboto_Mono } from 'next/font/google';
 import localFont from 'next/font/local';
 
+import { BASE_URL } from '@/const';
+
+export const metadata = {
+  title: '메이커스 블로그',
+  description: '',
+  openGraph: {
+    title: '메이커스 블로그',
+    description: 'SOPT Makers가 만들어가는 블로그입니다.',
+    url: BASE_URL ?? undefined,
+    type: 'website',
+  },
+  metadataBase: BASE_URL ? new URL(BASE_URL) : undefined,
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      'index': false,
+      'follow': true,
+      'noimageindex': true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
+
 const suitFont = localFont({
   src: [
     {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,8 @@
-import { BASE_URL } from '@/const';
 import { wrapAsyncComponent } from '@/util';
 
 import ArticleList from './category/[category]/page';
 
 const WrappedArticleList = wrapAsyncComponent(ArticleList);
-
-export const metadata = {
-  title: '메이커스 블로그',
-  description: '',
-  openGraph: {
-    title: '메이커스 블로그',
-    description: 'SOPT Makers가 만들어가는 블로그입니다.',
-    url: BASE_URL ?? undefined,
-    type: 'website',
-  },
-  metadataBase: BASE_URL ? new URL(BASE_URL) : undefined,
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      'index': false,
-      'follow': true,
-      'noimageindex': true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-};
 
 export default async function Home() {
   return <WrappedArticleList params={{ category: undefined }} />;
